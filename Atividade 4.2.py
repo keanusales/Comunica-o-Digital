52319d6bc101381ae6d472c05814e82f0cdf23a4
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2024-10-01 19:22:15 -0300

    Update Atividade 4.2.py

--- Atividade 4.2.py
+++ Atividade 4.2.py
@@ -1,6 +1,4 @@
-from timeit import timeit
-
-def sender(data: int, gen: int):
+def transmit(data: int, gen: int):
   orddt = data.bit_length() - 1
   ordgn = gen.bit_length() - 1
   sumord = orddt + ordgn
@@ -21,12 +19,9 @@ def receiver(data: int, gen: int):
 
 def main():
   data, gen = 0b1101011011, 0b10011
-  res = sender(data, gen)
-  if res := receiver(res, gen):
-    raise BufferError(f"{res} != 0")
+  res = transmit(data, gen)
+  print(f"Resposta: {bin(res)}")
+  res = receiver(res, gen)
+  print(f"Checagem: {bin(res)}")
 
-if __name__ == "__main__":
-  reps = 1_000_000
-  time = timeit(main, number = reps)
-  print(f"Média: {time / reps} seg.")
-  print(f"Total: {time} seg.")
+if __name__ == "__main__": main()
